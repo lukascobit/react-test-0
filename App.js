9b6f327fdef55468163df847d11d7d84bbec4d29
Lukas <77848587+lukascobit@users.noreply.github.com>
2021-04-19 15:38:12 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+
+/*
+function App() {
+  const num = 45
+  return (
+    <div className="App">
+      <h1>hello world {num}</h1>
+    </div>
+  );
+}*/
+
+class App extends React.Component {
+  render(){
+      return <h1>Component</h1>;
+  }
+}
+
+export default App;
